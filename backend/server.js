fc8839e876eb2c4a6bf184ac53f023e5940903ce
koushith <koushith97@gmail.com>
2021-04-09 00:11:50 +0530

    adds redux

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
