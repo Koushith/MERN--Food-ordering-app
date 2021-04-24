fca0c26841f76f4403171df94d192e2b6e103a21
koushith <koushith97@gmail.com>
2021-04-24 21:59:53 +0530

    adds node auth

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,41 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
+import {
+  getProduct,
+  getProductById,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
 // @desc Fetch all data
 //  @route GET /api/products
 //  @access Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+// router.get(
+//   '/',
+//   getProduct
+// );
+
+router.route('/').get(getProduct);
 
 // @desc Fetch all data
 //  @route GET /api/products/:id
 //  @access Public
 
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    console.log(product);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found.....');
-    }
-  })
-);
+// router.get(
+//   '/:id',
+//   asyncHandler(async (req, res) => {
+//     const product = await Product.findById(req.params.id);
+//     console.log(product);
+//     if (product) {
+//       res.json(product);
+//     } else {
+//       res.status(404);
+//       throw new Error('Product not found.....');
+//     }
+//   })
+// );
+
+router.route('/:id').get(getProductById);
 
 export default router;
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -11,20 +11,17 @@ import {
   Card,
 } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cartAction';
+import { addToCart, removeFromCart } from '../actions/cartAction';
 
-function CartScreen({ match, location, history }) {
+const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
 
-  const qty = location.search ? location.search.split('=')[1] : 1;
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
 
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
-
   const { cartItems } = cart;
-
-  console.log(cartItems);
 
   useEffect(() => {
     if (productId) {
@@ -32,8 +29,8 @@ function CartScreen({ match, location, history }) {
     }
   }, [dispatch, productId, qty]);
 
-  const removeFromCartHandler = () => {
-    console.log('removed');
+  const removeFromCartHandler = (id) => {
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
@@ -44,10 +41,9 @@ function CartScreen({ match, location, history }) {
     <Row>
       <Col md={8}>
         <h1>Shopping Cart</h1>
-
         {cartItems.length === 0 ? (
           <Message>
-            Your Cart is Empty<Link to='/'>Go Back</Link>{' '}
+            Your cart is empty <Link to='/'>Go Back</Link>
           </Message>
         ) : (
           <ListGroup variant='flush'>
@@ -93,8 +89,34 @@ function CartScreen({ match, location, history }) {
           </ListGroup>
         )}
       </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
     </Row>
   );
-}
+};
 
 export default CartScreen;
