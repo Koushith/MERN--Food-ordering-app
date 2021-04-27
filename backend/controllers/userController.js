a2f944fde4ae1f5fba941686f7c0c0e906446bfa
koushith <koushith97@gmail.com>
2021-04-27 20:02:14 +0530

    adds user auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,13 +8,23 @@ import User from '../models/userModel.js';
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
+  const user = await User.findOne({ email });
+
   //   get the username and password
   // cif exist compare it with database and return the token
 
-  res.json({
-    email,
-    password,
-  });
+  if (user && (await User.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Email or Password doesnot match');
+  }
 });
 
 export { authUser };
