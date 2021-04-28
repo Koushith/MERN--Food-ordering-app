a9fa4db0e009c805ee5b42aa308fce87e5adfc0b
koushith <koushith97@gmail.com>
2021-04-28 18:24:49 +0530

    user auth

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,6 +30,17 @@ const userSchema = mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+// it will run before create- and check if it creating new one or updating existing
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
