080dfe2e6152030ee965e5b497da1b08a88db4c0
koushith <koushith97@gmail.com>
2021-05-19 00:19:51 +0530

    completes backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,40 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc register user and get token
+//  @route Post /api/users
+//  @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(401);
+    throw new Error('user already exist');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  //  user created successfully
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('something went wrong, invalid user data');
+  }
+});
+
 // @desc send token in header from frontend and return loggedin user
 //  @route GET /api/user/profile
 //  @access Private
@@ -46,4 +80,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
